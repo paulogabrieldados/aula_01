f39835883b544b2388ca17c6d11df10a1e595b16
paulo.gomes <paulogabrieldados@gmail.com>
2025-05-26 17:13:57 -0300

    deu ruim

---
+++ main.py
@@ -0,0 +1,9 @@
+##print(input("Digite seu nome aqui: "))
+
+##print("Olá, " + input("Digite seu nome aqui: ") + "!")
+
+##print(len(input("Digite seu nome aqui: ")))	
+
+valor = (int(input("Inclua o valor"))  + int(input("Inclua o valor")))
+print(valor)
+
